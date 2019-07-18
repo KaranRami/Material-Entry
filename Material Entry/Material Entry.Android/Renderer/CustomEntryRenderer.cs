ccc1e13af2a908d6c7fd72783b5b5ff881cb37de
Karan Rami <32388574+KaranRami@users.noreply.github.com>
2019-07-18 23:55:32 +0530

    Custom Entry Render added for Android

---
+++ Material Entry/Material Entry.Android/Renderer/CustomEntryRenderer.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.Content.Res;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Material_Entry.Droid.Renderer;
+using Xamarin.Forms;
+using Xamarin.Forms.Material.Android;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(Entry), typeof(CustomEntryRenderer), new[] { typeof(VisualMarker.MaterialVisual) })]
+namespace Material_Entry.Droid.Renderer
+{
+    public class CustomEntryRenderer : MaterialEntryRenderer
+    {
+        public CustomEntryRenderer(Context context) : base(context)
+        {
+        }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
+        {
+            base.OnElementChanged(e);
+            Control.EditText.LayoutChange += EditText_LayoutChange;
+        }
+
+        private void EditText_LayoutChange(object sender, LayoutChangeEventArgs e)
+        {
+            if (Control != null)
+            {
+                Control.EditText.SetPadding(0, Control.EditText.PaddingTop, Control.EditText.PaddingRight, Control.EditText.PaddingBottom);
+                if (Control.EditText.IsFocused)
+                    Control.EditText.BackgroundTintList = ColorStateList.ValueOf(Android.Graphics.Color.Blue);
+            }
+        }
+
+        protected override void OnElementPropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            base.OnElementPropertyChanged(sender, e);
+            if (Control != null)
+            {
+                Control.EditText.SetPadding(0, Control.EditText.PaddingTop, Control.EditText.PaddingRight, Control.EditText.PaddingBottom);
+                if (e.PropertyName.ToUpper() == "ISFOCUSED" && !Control.EditText.IsFocused)
+                    Control.EditText.BackgroundTintList = ColorStateList.ValueOf(Element.TextColor.ToAndroid());
+            }
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            Control.EditText.LayoutChange -= EditText_LayoutChange;
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Material Entry/Material Entry.iOS/Renderer/CustomEntryRenderer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+
+using Foundation;
+using Material_Entry.iOS.Renderer;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Material.iOS;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(Entry), typeof(CustomEntryRenderer), new[] { typeof(VisualMarker.MaterialVisual) })]
+namespace Material_Entry.iOS.Renderer
+{
+    public class CustomEntryRenderer : MaterialEntryRenderer
+    {
+
+        protected override void OnElementPropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            base.OnElementPropertyChanged(sender, e);
+        }
+        protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
+        {
+            base.OnElementChanged(e);
+            if (Control != null)
+                Console.WriteLine("Still in progress");
+        }
+    }
+}
